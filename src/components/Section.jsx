8f13e4792132c9fbd3dbe1803ef989775c985bb4
fhoinaski <sukinodoncai@gmail.com>
2023-04-11 20:47:28 -0300

    update botão

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -8,7 +8,9 @@ const Section = () => {
   return (
     <div className='p-2 md:p-3  w-full h-44 md:h-auto  bg-green-100'>
       <img className='md:w-40 w-32 absolute md:right-8 right-2 xl:w-48 xl:right-60 top-16 z-10' src="/maquininhasTonCard1.webp" alt="máquinas ton" />
-      <button className="bg-secondary hover:bg-btn_primary rounded-full px-10 py-1 absolute z-10 top-48 mx-16 md:mx-0  text-gray-900 md:right-60 md:top-44 xl:right-1/3 xl:top-48">Aprovite agora</button>
+      {/* <button className="bg-secondary hover:bg-btn_primary rounded-full px-10 py-1 absolute z-10 top-48 mx-auto md:mx-0 left-0 right-0 text-gray-900 md:right-60 md:top-44 xl:right-1/3 xl:top-48">Aproveite agora</button>
+ */}
+      <button className="bg-secondary hover:bg-btn_primary rounded-full px-10 py-1 absolute z-10 top-48 mx-auto md:mx-0 left-10 right-10 md:left-auto text-gray-900 md:right-60 md:top-44 xl:right-1/3 xl:top-48">Aprovite agora</button>
       <div className="rounded-xl relative h-28 md:h-auto bg-color_card py-8 w-full md:w-11/12 lg:w-9/12 mx-auto">
         <div className="absolute inset-0 overflow-hidden">
 
